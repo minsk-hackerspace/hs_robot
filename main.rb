a14ae54212145cfe8d04bbd5862d34d032d1fd6b
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2015-04-19 02:45:53 +0300

    class for app

--- main.rb
+++ main.rb
@@ -6,37 +6,40 @@ require_relative 'robot/robot'
 require_relative 'robot/gpio'
 require_relative 'robot/pin'
 
-get '/init' do
-  @@robot ||= Robot.new
-end
-
-get '/' do
-  haml :index, layout: :main
-end
-
-get '/answer' do
-  command = 's'
-  case params[:keys].to_i
-    when 83
-      command = 's'
-      @@robot.forward
-    when 87
-      command = 'w'
-      @@robot.backward
-    when 68
-      command = 'd'
-      @@robot.right
-    when 65
-      command = 'a'
-      @@robot.left
-    when 32
-      command = ' '
-      @@robot.stop
+class HS_Robot < Sinatra::Application
+  get '/init' do
+    @robot ||= Robot.new
   end
 
-  command
-end
+  get '/' do
+    haml :index, layout: :main
+  end
 
-get '/destroy' do
-  @@robot.destroy
+  get '/answer' do
+    command = 's'
+    case params[:keys].to_i
+      when 83
+        command = 's'
+        @robot.forward
+      when 87
+        command = 'w'
+        @robot.backward
+      when 68
+        command = 'd'
+        @robot.right
+      when 65
+        command = 'a'
+        @robot.left
+      when 32
+        command = ' '
+        @robot.stop
+    end
+
+    command
+  end
+
+  get '/destroy' do
+    @robot.destroy
+  end
+
 end
