75bcbbcace27822c7aee9a8a8336c5c91fb4dc3a
Alex Gutikov <gutikoff@gmail.com>
2015-04-19 14:40:40 +0300

    rpi: video: lag=10..14 sec.

--- test/videostreaming-gst.sh
+++ test/videostreaming-gst.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 
-raspivid -t 0 -h 180 -w 320 -fps 4 -b 200000 -o - | \
+raspivid --timeout 0 --height 180 --width 320 --framerate 5 --bitrate 1000000 --output - | \
 gst-launch-0.10 -v fdsrc ! h264parse ! ffdec_h264 ! \
 queue ! \
-theoraenc drop-frames=true ! oggmux ! \
+theoraenc bitrate=1000000 drop-frames=true keyframe-auto=false keyframe-force=5 speed-level=2 vp3-compatible=true ! \
+oggmux ! \
 tcpserversink host=192.168.100.217 port=8080
 
 
