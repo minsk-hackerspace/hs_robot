44c194e763f87ba83c07e5610d0ee888d7de5fd1
Alex Gutikov <gutikoff@gmail.com>
2015-04-28 18:47:35 +0300

    iframe 620x420

--- video/gst_stream.sh
+++ video/gst_stream.sh
@@ -13,6 +13,13 @@ v4l2-ctl --set-fmt-video=width=$WIDTH,height=$HEIGHT,pixelformat=2
 # set framerate
 v4l2-ctl --set-parm=$FPS
 
+# magic parapeters
+# v4l2-ctl --set-ctrl=auto_exposure=0
+# v4l2-ctl --set-ctrl=exposure_dynamic_framerate=1
+# v4l2-ctl --set-ctrl=iso_sensitivity=4
+# v4l2-ctl --set-ctrl=scene_mode=night
+
+
 # read stream by v4l2-ctl because gstreamer's v4l2src will reset frame settings to default
 # capture stream-count frames in about stream-count/framerate seconds
 v4l2-ctl --stream-mmap=3 --stream-count=5000 --stream-to=- \
