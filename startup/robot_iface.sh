9015c0d348453c44eb9797e4ff32e0d0b25eef25
Alex Gutikov <gutikoff@gmail.com>
2015-04-28 09:43:33 +0300

    startup scripts

--- startup/robot_iface.sh
+++ startup/robot_iface.sh
@@ -1,21 +1,62 @@
 #!/bin/sh
 
-WEB_HOME=/home/pi/hs_robot/web
+ROBOT_HOME=`dirname $0`/../
 
+CONNECTOR_SESSION=$(screen -ls | grep connector | awk '{print $1}')
 
-restart_sinatra () {
+MAIN_SESSION=$(screen -ls | grep main | awk '{print $1}')
 
-	ruby $WEB_HOME/connector.rb &
+stop_sinatra () {
 
-	CONNECTOR_PID=$!
+	if [ "$CONNECTOR_SESSION" != "" ]; then
+		echo "hs_robot: stop connector, session: $CONNECTOR_SESSION"
+		screen -S $CONNECTOR_SESSION -X quit
+	else
+		echo "hs_robot: connector not started"
+	fi
 
-	ruby $WEB_HOME/main.rb -o 0.0.0.0 -p 8081 &
-
-	MAIN_PID=$!
+	if [ "$MAIN_SESSION" != "" ]; then
+		echo "hs_robot: stop main, session: $MAIN_SESSION"
+		screen -S $MAIN_SESSION -X quit
+	else
+		echo "hs_robot: main not started"
+	fi
 
 }
 
+start_sinatra () {
 
-restart_sinatra
+	if [ "$CONNECTOR_SESSION" != "" ]; then
+		echo "hs_robot: connector is running, session: $CONNECTOR_SESSION"
+	else
+		echo "hs_robot: starting connector"
+		screen -c $ROBOT_HOME/startup/screenrc/connector.screenrc -S connector -d -m ruby $ROBOT_HOME/web/connector.rb
+	fi
+
+	if [ "$MAIN_SESSION" != "" ]; then
+		echo "hs_robot: main is running, session: $MAIN_SESSION"
+	else
+		echo "hs_robot: starting main"
+		screen -c $ROBOT_HOME/startup/screenrc/main.screenrc -S main -d -m ruby $ROBOT_HOME/web/main.rb -o 0.0.0.0 -p 8081
+	fi
+
+}
+
+case "$1" in
+	start)
+		start_sinatra
+		;;
+	stop) 
+		stop_sinatra
+		;;
+	restart)
+		stop_sinatra
+		start_sinatra
+		;;
+	*)
+		echo "Usage: $0 start|stop|restart"
+		exit 1
+		;;
+	esac
 
 
--- startup/robot_video.sh
+++ startup/robot_video.sh
@@ -1,20 +1,62 @@
+#!/bin/sh
 
-VIDEO_HOME=/home/pi/hs_robot/video
+ROBOT_HOME=`dirname $0`/../
 
+GST_SESSION=$(screen -ls | grep gst | awk '{print $1}')
 
+MJPEG_SESSION=$(screen -ls | grep mjpeg | awk '{print $1}')
 
+stop_video () {
 
-restart_video () {
+	if [ "$GST_SESSION" != "" ]; then
+		echo "hs_robot: stop gst_stream, session: $GST_SESSION"
+		screen -S $GST_SESSION -X quit
+	else
+		echo "hs_robot: gst_stream not started"
+	fi
 
-	$VIDEO_HOME/mjpeg_http_server.py &
-
-	MJPG_SERVER_PID=$!
-
-	$VIDEO_HOME/gst_stream.sh &
-
-	GST_STREAM_PID=$!
+	if [ "$MJPEG_SESSION" != "" ]; then
+		echo "hs_robot: stop mjpeg_http_server, session: $MJPEG_SESSION"
+		screen -S $MJPEG_SESSION -X quit
+	else
+		echo "hs_robot: mjpeg_http_server not started"
+	fi
 
 }
 
+start_video () {
 
-restart_video
+	if [ "$GST_SESSION" != "" ]; then
+		echo "hs_robot: gst is running, session: $GST_SESSION"
+	else
+		echo "hs_robot: starting gst_stream"
+		screen -c $ROBOT_HOME/startup/screenrc/gst.screenrc -S gst -d -m  $ROBOT_HOME/video/gst_stream.sh
+	fi
+
+	if [ "$MJPEG_SESSION" != "" ]; then
+		echo "hs_robot: mjpeg is running, session: $MJPEG_SESSION"
+	else
+		echo "hs_robot: starting mjpeg_http_server"
+		screen -c $ROBOT_HOME/startup/screenrc/mjpeg.screenrc -S mjpeg -d -m  $ROBOT_HOME/video/mjpeg_http_server.py
+	fi
+
+}
+
+case "$1" in
+	start)
+		start_video
+		;;
+	stop) 
+		stop_video
+		;;
+	restart)
+		stop_video
+		start_video
+		;;
+	*)
+		echo "Usage: $0 start|stop|restart"
+		exit 1
+		;;
+	esac
+
+
