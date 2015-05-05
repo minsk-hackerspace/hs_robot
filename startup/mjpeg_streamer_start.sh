276974b6bd2b94fe61746342206b02676349e860
Alex Gutikov <gutikoff@gmail.com>
2015-05-05 13:28:10 +0300

    mjpeg parameters changed

--- startup/mjpeg_streamer_start.sh
+++ startup/mjpeg_streamer_start.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-MJPEG_HOME=~/mjpg-streamer/mjpg-streamer-experimental/
+MJPEG_HOME=/home/pi/mjpg-streamer/mjpg-streamer-experimental/
 
 
 cd $MJPEG_HOME
 
 export LD_LIBRARY_PATH=.
 
-./mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -x 600 -y 400 -fps 5"
+./mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -x 600 -y 400 -fps 16 -quality 20"
 
